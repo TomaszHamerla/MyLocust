71039f15c4d43ed3111ed3a4b511c8db6a4c9c87
unknown <tomaszhamerla@interia.pl>
2024-05-25 10:49:26 +0200

    initial

---
+++ main.py
@@ -0,0 +1,39 @@
+from locust import HttpUser, task, between, TaskSet
+from requests.auth import HTTPBasicAuth
+
+
+class UserBehavior(TaskSet):
+    BASE_URL="api/v1/"
+    def on_start(self):
+        # Endpoint logowania
+        login_url = f"{self.BASE_URL}users/login"
+        # Dane logowania
+        username = "Doe"
+        password = "456"
+
+        # Wysłanie żądania logowania z Basic Auth
+        response = self.client.post(login_url, auth=HTTPBasicAuth(username, password))
+        if response.status_code == 200:
+            self.token = response.json().get("token")
+        else:
+            print("Login failed:", response.text)
+            self.token = None
+
+    @task(1)
+    def users(self):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        self.client.get(f"{self.BASE_URL}users", headers=headers)
+
+    @task(1)
+    def brands(self):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        self.client.get(f"{self.BASE_URL}cars/brands", headers=headers)
+
+    @task(1)
+    def models(self):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        self.client.get(f"{self.BASE_URL}cars/brands/models/3", headers=headers)
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
